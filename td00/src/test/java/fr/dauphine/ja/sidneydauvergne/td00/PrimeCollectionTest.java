9f893c09f4e7332650b8d4034c142862d4e40edc
dauvsi16 <dauvsi16@b032-05.crio.dauphine.fr>
2019-10-14 17:53:50 +0200

    Ajout de PrimeCollectiontest.java

---
+++ td00/src/test/java/fr/dauphine/ja/sidneydauvergne/td00/PrimeCollectionTest.java
@@ -0,0 +1,53 @@
+package fr.dauphine.ja.sidneydauvergne.td00;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class PrimeCollectionTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PrimeCollectionTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+   /* public static Test suite()
+    {
+        return new TestSuite( PrimeCollection.class );
+    }*/
+
+    /**
+     * Rigourous Test :-)
+     */
+    /*public void testApp()
+    {
+        assertTrue( true );
+    }*/
+    public void test0isPrime()
+    {
+        assertFalse(PrimeCollection.isPrime(0));
+    }
+    
+    public void testTwoIsPrime()
+    {
+        assertTrue(PrimeCollection.isPrime(2));
+    }
+    
+    public void test9IsNotPrime()
+    {
+        assertFalse(PrimeCollection.isPrime(9));
+    }    
+    
+}
