7fdeaf97eb87886ee93152c94e8206d1e43240b1
dauvsi16 <dauvsi16@b028-11.crio.dauphine.fr>
2019-10-25 15:06:02 +0200

    Fin exercice 1 td mvc

--- shapes/src/main/java/fr/dauphine/ja/sidneydauvergne/view/MyDisplay.java
+++ shapes/src/main/java/fr/dauphine/ja/sidneydauvergne/view/MyDisplay.java
@@ -1,10 +1,17 @@
 package fr.dauphine.ja.sidneydauvergne.view;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.*;
 
 public class MyDisplay extends JPanel {
+
+	public void paintComponent(Graphics g) {
+		g.drawOval(100,100,100,100);	
+		g.drawString("Cercle",300,100);
+	}
+	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Java Avancé - Graphic Display");
 		frame.setSize(new Dimension(500,500));
