3a1742084dc812abd8e47d5a5ca8008c73c591ec
Sidney <sidney.dauvergne@dauphine.eu>
2019-12-13 16:13:56 +0100

    panel1(int,int) with anonymous class

--- iterables/src/main/java/fr/dauphine/ja/sidneydauvergne/iterables/Panel.java
+++ iterables/src/main/java/fr/dauphine/ja/sidneydauvergne/iterables/Panel.java
@@ -4,20 +4,33 @@ import java.util.*;
 
 public class Panel implements Iterable<Integer>{
 
+	//private int beginning,end;
+	
 	public Iterator<Integer> iterator() {
 		return panel1(0,0);
 	}
 	
-	public static Iterator<Integer> panel1(int debut, int fin) {
-		ArrayList<Integer> a = new ArrayList<Integer>();
-		for(int i=debut;i<=fin;i++) {
-			a.add(i);
-		}
-		return a.iterator();
+	public static Iterator<Integer> panel1(final int beginning, final int end) {
+		return new Iterator<Integer>() {
+			int cursor = beginning;
+			
+			public boolean hasNext() {
+				if (cursor<=end) {
+					return true;
+				}
+				return false;
+			}
+			
+			public Integer next() {
+				return cursor++;
+			}
+		};
+			
 	}
 	
+	
 	public static void main( String[] args ){
-		 Iterator<Integer> it = panel1(1,5); 
+		 Iterator<Integer> it = panel1(3,10); 
 		 for(;it.hasNext();)
 			 System.out.println(it.next());
 	}
